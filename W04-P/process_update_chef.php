c871d6a8e22ab56fdd7141399fcaf9950ee6b1e0
TTaemin <70924137+TTaemin@users.noreply.github.com>
2020-10-05 01:05:56 +0900

    Create process_update_chef.php

---
+++ W04-P/process_update_chef.php
@@ -0,0 +1,24 @@
+<?php
+    $link = mysqli_connect('localhost', 'root', 'rootroot', 'dbp');
+    settype($_POST['id'], 'integer');
+    $filtered = array(
+        'id' => mysqli_real_escape_string($link, $_POST['id']),
+        'name' => mysqli_real_escape_string($link, $_POST['name']),
+        'country' => mysqli_real_escape_string($link, $_POST['country'])
+    );
+    $query = "
+        UPDATE chef
+            SET
+                name = '{$filtered['name']}',
+                country = '{$filtered['country']}'
+            WHERE
+                id = '{$filtered['id']}'
+    ";
+
+    $result = mysqli_query($link, $query);
+    if ($result == false) {
+        echo '수정하는 과정에서 문제가 생겼습니다. 관리자에게 문의해주세요';
+        error_log(mysqli_error($link));
+    } else {
+        header('Location: chef.php');
+    }
